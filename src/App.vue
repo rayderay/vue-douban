d46e6d026d47c7bd8718d8b38cd3e0059a971291
yanrui <yanrui@nonobank.com>
2018-09-14 13:55:32 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<div class="id">
+  <router-view ></router-view>
+	<div class="tab">
+		<div class="tab-item">
+			<router-link to='/home' class="item">
+				<img class="icon"  src="./common/img/bottom/icon-home.png">
+				<p class="text">首页</p>
+			</router-link>
+		</div>
+		<div class="tab-item" >
+			<router-link to="/book" class="item">
+				<img src="./common/img/bottom/icon-book.png">
+				<p class="text">书影音</p>
+			</router-link>
+		</div>
+		<div class="tab-item">
+			<router-link to="/mine" class="item">
+				<img src="./common/img/bottom/home-mine.png">
+				<p class="text">我的</p>
+			</router-link>
+		</div>
+	</div>
+</div>
+	
 </template>
 
+
 <script>
-export default {
-  name: 'App'
-}
+export default {};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+.tab {
+  position: fixed;
+  display: flex;
+  bottom: 0px;
+  left: 0px;
+  height: 40px;
+  width: 100%;
+  border-top: 1px solid #e6e6e6;
+  .tab-item {
+    flex: 1;
+    text-align: center;
+    .item {
+      display: inline-block;
+      width: 100px;
+      .text {
+        color: #a09b9b;
+        font-size: 10px;
+        -webkit-transform: scale(0.8);
+        margin-top: -11px;
+      }
+    }
+  }
 }
 </style>
+
---
+++ src/components/bottom/bottom.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="bottom">
+		
+	</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/img/bottom/iconfont.css';
+.bottom {
+  flex: 1;
+}
+</style>
+
+
