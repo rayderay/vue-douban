0484e35e174c146bbba58ee3c030f5ca794d5397
yanrui <yanrui@nonobank.com>
2018-12-19 17:03:14 +0800

    update type components

---
+++ src/components/Types.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="movieTypes">
+    <h2>分类浏览</h2>
+    <div class="content">
+      <ul>
+        <li v-for="item in items">
+          <a :href=" baseUrl + item.href">
+            {{item.title}}
+            <span></span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
